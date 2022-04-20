bd2f54a3d0334668d5385e57fe2ce820cca5ba3b
Rustam <gazizov_rustam@inbox.ru>
2022-04-20 15:53:25 +1000

    тест 2

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) {
         try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+
             String line = reader.readLine();
             String[] mass = line.split(" ");
             if(mass.length > 3) throw new IOException("incorrect data format");
